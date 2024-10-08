7ad71d02431b870ec428851d2abc89ed0b95d2c6
Ahmed <132069136+ma9737149@users.noreply.github.com>
2024-10-08 08:33:14 +0300

    Update github_view.py
    Signed-off-by: Ahmed <132069136+ma9737149@users.noreply.github.com>

--- view/github_view.py
+++ view/github_view.py
@@ -4,9 +4,9 @@ import requests
 class Pagenation_System_View(discord.ui.View):
     def __init__(self,page:int,user_name,user_repos_json_data:dict,user_id):
         super().__init__(timeout=None)
-        self.p = page
-        self.r = user_repos_json_data
-        self.n = user_name
+        self.page = page
+        self.repos = user_repos_json_data
+        self.name = user_name
         self.id = user_id
 
     def calc_perecentege(self,dictionary:dict) -> list:
@@ -24,18 +24,21 @@ class Pagenation_System_View(discord.ui.View):
 
 
     async def return_to_first_page_func(self,interaction:discord.Interaction,embed:discord.Embed) -> None:
-        user_repos_json_data = requests.get(f"https://api.github.com/users/{self.n}/repos").json()
-        link = requests.get(f"https://api.github.com/users/{self.n}")
+        user_repos_json_data = requests.get(f"https://api.github.com/users/{self.name}/repos").json()
+        link = requests.get(f"https://api.github.com/users/{self.name}")
         json_link = link.json()
-        bio = "Therse Is No Bio" if json_link["bio"] == None else json_link["bio"]
+
+        bio = json_link.get("bio" , "Therse Is No Bio")
         user_id = json_link["id"]
         avatar_url = json_link["avatar_url"]
+
         followers = json_link["followers"]
         following = json_link["following"]
         public_repos = json_link["public_repos"]
-        name = json_link["name"] if json_link["name"] != None else "There Is No Name"
+        name = json_link.get("name" , "There Is No Name")
         joined_at = str(json_link["created_at"]).split("T")[0]
         updated_at = str(json_link["updated_at"]).split("T")[0]
+
         embed.title = f"{name} Github Info"
         embed.description = f"> User Bio : {bio}\n> User Id : {user_id}\n> Followers : {followers}\n> Following : {following}\n> Public Repos : {public_repos}\n> Name : {name}\n> Joined at : {joined_at}\n> Updated At : {updated_at}"
         embed.set_thumbnail(url=avatar_url)
@@ -55,28 +58,28 @@ class Pagenation_System_View(discord.ui.View):
 
         await interaction.response.defer()
         embed = interaction.message.embeds[0]
-        self.p -= 1
+        self.page -= 1
 
-        if self.p < 0:
-            self.p = len(self.r)
+        if self.page < 0:
+            self.page = len(self.repos)
 
-        elif self.p == 0:
+        elif self.page == 0:
             await self.return_to_first_page_func(interaction,embed)
             return
 
 
-        name = self.r[self.p - 1]["name"]
-        url = self.r[self.p - 1]["svn_url"]
-        created_at = str(self.r[self.p - 1]["created_at"]).split("T")[0]
-        size = self.r[self.p - 1]["size"] / 1000
-        language = self.r[self.p - 1]["language"]
-        stars = self.r[self.p - 1]["stargazers_count"]
+        name = self.repos[self.page - 1]["name"]
+        url = self.repos[self.page - 1]["svn_url"]
+        created_at = str(self.repos[self.page - 1]["created_at"]).split("T")[0]
+        size = self.repos[self.page - 1]["size"] / 1000
+        language = self.repos[self.page - 1]["language"]
+        stars = self.repos[self.page - 1]["stargazers_count"]
 
-        my_data = self.calc_perecentege(requests.get(f"https://api.github.com/repos/{self.n}/{name}/languages").json())
+        my_data = self.calc_perecentege(requests.get(f"https://api.github.com/repos/{self.name}/{name}/languages").json())
 
 
         embed.description = f"Name : {name}\nRepo Url : {url}\nCreated At : {created_at}\nSize : {size}MB\nLanguage : {language}\nStars : {stars}\n" + "\n".join(my_data)
-        embed.set_footer(text=f"Page : {self.p} / {len(self.r)}")
+        embed.set_footer(text=f"Page : {self.page} / {len(self.repos)}")
 
         await interaction.message.edit(embed=embed)
 
@@ -85,29 +88,30 @@ class Pagenation_System_View(discord.ui.View):
         if interaction.user.id != self.id:
             await interaction.response.send_message(f"You can't use this embed because it's controlled by <@{self.id}>",ephemeral=True)
             return
+
         await interaction.response.defer()
         embed = interaction.message.embeds[0]
-        self.p += 1
+        self.page += 1
 
-        if self.p > len(self.r):
-            self.p = 0
+        if self.page > len(self.repos):
+            self.page = 0
 
-        if self.p == 0:
+        if self.page == 0:
             await self.return_to_first_page_func(interaction,embed)
             return
 
 
 
-        name = self.r[self.p - 1]["name"]
-        url = self.r[self.p - 1]["svn_url"]
-        created_at = str(self.r[self.p - 1]["created_at"]).split("T")[0]
-        size = self.r[self.p - 1]["size"] / 1000
-        language = self.r[self.p - 1]["language"]
-        stars = self.r[self.p - 1]["stargazers_count"]
-        my_data = self.calc_perecentege(requests.get(f"https://api.github.com/repos/{self.n}/{name}/languages").json())
+        name = self.repos[self.page - 1]["name"]
+        url = self.repos[self.page - 1]["svn_url"]
+        created_at = str(self.repos[self.page - 1]["created_at"]).split("T")[0]
+        size = self.repos[self.page - 1]["size"] / 1000
+        language = self.repos[self.page - 1]["language"]
+        stars = self.repos[self.page - 1]["stargazers_count"]
+        my_data = self.calc_perecentege(requests.get(f"https://api.github.com/repos/{self.name}/{name}/languages").json())
 
 
         embed.description = f"Name : {name}\nRepo Url : {url}\nCreated At : {created_at}\nSize : {size}MB\nLanguage : {language}\nStars : {stars}\n" + "\n".join(my_data)
-        embed.set_footer(text=f"Page : {self.p} / {len(self.r)}")
+        embed.set_footer(text=f"Page : {self.page} / {len(self.repos)}")
 
         await interaction.message.edit(embed=embed)
