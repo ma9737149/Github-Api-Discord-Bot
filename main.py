20f61922f390884d20546c304e1d78aeb226a24d
Ahmed <132069136+ma9737149@users.noreply.github.com>
2024-10-08 08:34:07 +0300

    Update main.py
    Signed-off-by: Ahmed <132069136+ma9737149@users.noreply.github.com>

--- main.py
+++ main.py
@@ -29,15 +29,19 @@ async def _github_info(interaction: discord.Interaction, user_name: str) -> None
         user_repos_json_data = requests.get(f"https://api.github.com/users/{user_name}/repos").json()
         link = requests.get(f"https://api.github.com/users/{user_name}")
         json_link = link.json()
+
         bio = json_link.get("bio" , "Therse Is No Bio")
         user_id = json_link["id"]
         avatar_url = json_link["avatar_url"]
+
         followers = json_link["followers"]
         following = json_link["following"]
+
         public_repos = json_link["public_repos"]
         name = json_link.get("name" , "There Is No Name")
         joined_at = str(json_link["created_at"]).split("T")[0]
         updated_at = str(json_link["updated_at"]).split("T")[0]
+
         embed = discord.Embed(title=f"{user_name} Github Info", description=f"> User Bio : {bio}\n> User Id : {user_id}\n> Followers : {followers}\n> Following : {following}\n> Public Repos : {public_repos}\n> Name : {name}\n> Joined at : {joined_at}\n> Updated At : {updated_at}", color=discord.Colour.dark_gold())
         embed.set_thumbnail(url=avatar_url)
         embed.set_footer(
@@ -49,6 +53,7 @@ async def _github_info(interaction: discord.Interaction, user_name: str) -> None
 
     except Exception as e:
         await interaction.response.send_message(f"Failled To Get This User Info", ephemeral=True)
+
 
 
 
